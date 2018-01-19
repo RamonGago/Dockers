059534cdfb411bed9b931215df129e160803b095
RamonGago <ramongagocarrera@gmail.com>
2018-01-19 18:42:27 +0100

    Eighth test

--- contenedores/microservice.py
+++ contenedores/microservice.py
@@ -1,18 +1,12 @@
 from flask import Flask, jsonify
 
-microservice = Flask(__name__)
+app = Flask(__name__)
 
-@microservice.route("/")
-def getRoot():
-    return jsonify(
-        status="OK",
-    )
-
-@microservice.route("/status")
+@app.route("/status")
 def getStatus():
     return jsonify(
         status="OK",
     )
 
 if __name__ == '__main__':
-    microservice.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
