72b76b1ff57467eaa774a96eec9acc885894cb74
RamonGago <ramongagocarrera@gmail.com>
2018-01-19 13:42:45 +0100

    Fifth test

--- contenedores/server.py
+++ contenedores/server.py
@@ -3,10 +3,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/status")
-def server_info():
-    return jsonify({
-        status: "OK"
-    })
+def getStatus():
+    return jsonify(
+        status="OK",
+    )
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
